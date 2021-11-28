a811ce8d7a8f15e357ec683a95e88c75a7e7810a
Lomanfan <lfan.canada@gmail.com>
2021-11-27 20:00:25 -0700

    update counter.jsx

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,38 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends React.Component {
   state = {
     count: 0,
-    tags: ['tag1', 'tag2', 'tag3']
+    tags: ["tag1", "tag2", "tag3"],
   };
 
-  handleIncrement = ()=> {
-    this.setState({count: this.state.count + 1})
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
   };
 
-  render() { 
+  render() {
     return (
-    <React.Fragment>
-    <span className={this.getBadgeClasses()}>{this.formatCount()}</span> 
-    <button onClick={() => this.handleIncrement()} className="btn btn-secondary btn-sm">Increment</button>
-    <ul>
-      {/* {this.state.tags.map((tag) => <li key={tag}>{tag}</li>)} */}
-    </ul>
-    </React.Fragment>
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.state.count}</span>  {/*We can also call function {this.formatCount()} instead of rendering {this.state.count}, destructure {count} out of this.state to further reduce repetitive code. */}
+        <button
+          onClick={() => this.handleIncrement()}
+          className="btn btn-primary btn-sm"
+        >
+          +
+        </button>
+        <ul>
+          {/* {this.state.tags.map((tag) => <li key={tag}>{tag}</li>)} */}
+        </ul>
+      </React.Fragment>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += (this.state.count === 0) ? "warning" : "primary";
+    classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
   }
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
-  }
-
+  // formatCount() {
+  //   const { count } = this.state;
+  //   return count === 0 ? "Zero" : count;
+  // }
 }
- 
+
 export default Counter;
