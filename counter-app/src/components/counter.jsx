08f650a089272493ce8d9daee1d655bd73d27ab6
Lomanfan <lfan.canada@gmail.com>
2021-11-29 22:56:34 -0700

    pass props from counters to counter

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends React.Component {
   state = {
-    count: 0,
+    count: this.props.value,
     tags: ["tag1", "tag2", "tag3"],
   };
 
