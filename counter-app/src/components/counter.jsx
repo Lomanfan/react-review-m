0cdd1c8fe3adc73a0cb5932e375ff03e19c84436
Lomanfan <lfan.canada@gmail.com>
2021-11-29 23:06:44 -0700

    change count to value for consistency

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,16 +2,16 @@ import React, { Component } from "react";
 
 class Counter extends React.Component {
   state = {
-    count: this.props.value,
+    value: this.props.value,
     tags: ["tag1", "tag2", "tag3"],
   };
 
   handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
-    console.log('props', this.props);
+    // console.log('props', this.props);
 
     return (
       <React.Fragment>
@@ -31,13 +31,13 @@ class Counter extends React.Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
