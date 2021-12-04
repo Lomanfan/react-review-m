4f085513ec9c7250a752024398419c1b8cce445e
Lomanfan <lfan.canada@gmail.com>
2021-12-04 14:33:36 -0700

    destructure props

--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -5,12 +5,14 @@ class Counters extends React.Component {
 
 
   render() {
+    const {onReset, counters, onDelete, onIncrement} = this.props;
+
     return (
       <div>
         <button 
-        onClick={this.props.onReset}
+        onClick={onReset}
         className="btn btn-primary btn-sm m-2">Reset</button>
-        {this.props.counters.map((counter) => (<Counter key={counter.id} onDelete={this.props.onDelete} onIncrement={this.props.onIncrement} counter={counter} selected={true}/>))}
+        {counters.map((counter) => (<Counter key={counter.id} onDelete={onDelete} onIncrement={onIncrement} counter={counter} selected={true}/>))}
       </div>
     );
   }
