333286e9ac91f1106292063e84ea83af7740543c
Lomanfan <lfan.canada@gmail.com>
2021-12-04 22:33:17 -0700

    add Bootstrap grid div.col-1+div.col

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -6,29 +6,35 @@ class Counter extends React.Component {
 
     return (
       <React.Fragment>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>{" "}
-        {/*Instead of rendering {this.state.count}, we call function {this.formatCount()} , destructure {count} out of this.state to further reduce repetitive code; and render "Zero" or count conditionally*/}
-        <button
-          onClick={() => this.props.onIncrement(this.props.counter)}
-          className="btn btn-primary btn-sm"
-        >
-          +
-        </button>
-        <button
-          onClick={() => this.props.onDecrement(this.props.counter)}
-          className="btn btn-primary btn-sm m-2"
-        >
-          -
-        </button>
-        <button
-          onClick={() => this.props.onDelete(this.props.counter.id)}
-          className="btn btn-danger btn-sm"
-        >
-          Delete
-        </button>
-        <ul>
-          {/* {this.state.tags.map((tag) => <li key={tag}>{tag}</li>)} */}
-        </ul>
+        <div className="row">
+          <div className="col-1">
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            {/*Instead of rendering {this.state.count}, we call function {this.formatCount()} , destructure {count} out of this.state to further reduce repetitive code; and render "Zero" or count conditionally*/}
+          </div>
+          <div className="col">
+            <button
+              onClick={() => this.props.onIncrement(this.props.counter)}
+              className="btn btn-primary btn-sm ml-2"
+            >
+              +
+            </button>
+            <button
+              onClick={() => this.props.onDecrement(this.props.counter)}
+              className="btn btn-primary btn-sm m-2"
+            >
+              -
+            </button>
+            <button
+              onClick={() => this.props.onDelete(this.props.counter.id)}
+              className="btn btn-danger btn-sm"
+            >
+              Delete
+            </button>
+          </div>
+          <ul>
+            {/* {this.state.tags.map((tag) => <li key={tag}>{tag}</li>)} */}
+          </ul>
+        </div>
       </React.Fragment>
     );
   }
