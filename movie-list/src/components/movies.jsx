fcffd92ba76a8ee0ff44273fc1efa4c0a2b61837
Lomanfan <lfan.canada@gmail.com>
2021-12-06 20:38:31 -0700

    handleDelete

--- movie-list/src/components/movies.jsx
+++ movie-list/src/components/movies.jsx
@@ -6,6 +6,11 @@ class Movies extends React.Component {
     movies: getMovies(), //Temporarily to initialize, to be updated.
   };
 
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({movies : movies});
+  }
+
   render() {
     console.log(this.state.movies);
     return (
@@ -16,15 +21,19 @@ class Movies extends React.Component {
             <th>Genre</th>
             <th>Stock</th>
             <th>Rate</th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
           {this.state.movies.map((movie) => (
-            <tr>
+            <tr key={movie._id}>
               <td>{movie.title}</td>
               <td>{movie.genre.name}</td>
               <td>{movie.numberInStock}</td>
               <td>{movie.dailyRentalRate}</td>
+              <td>
+                <button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button>
+              </td>
             </tr>
           ))}
         </tbody>
