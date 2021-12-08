08295fee5f8375685aa3d4c2933ea76ef8d5fc46
Lomanfan <lfan.canada@gmail.com>
2021-12-07 21:56:20 -0700

    change like to stateless functional component

--- movie-list/src/components/common/like.jsx
+++ movie-list/src/components/common/like.jsx
@@ -1,18 +1,17 @@
 import React, { Component } from "react";
 
-class Like extends React.Component {
-  render() {
-    let classes = "fa fa-heart";
-    if (!this.props.liked) classes += "-o";
-    return (
-      <i
-        onClick={this.props.onLikeToggle}
-        style={{ cursor: "pointer" }}
-        className={classes}
-        aria-hidden="true"
-      ></i>
-    );
-  }
-}
+const Like = (props) => {
+  let classes = "fa fa-heart";
+  if (!props.liked) classes += "-o";
+
+  return (
+    <i
+      onClick={props.onLikeToggle}
+      style={{ cursor: "pointer" }}
+      className={classes}
+      aria-hidden="true"
+    />
+  );
+};
 
 export default Like;
