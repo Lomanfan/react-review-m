723a796488022a73fc62dbec9762a24fa78e186d
Lomanfan <lfan.canada@gmail.com>
2021-12-07 21:38:21 -0700

    add cursor pointer style

--- movie-list/src/components/common/like.jsx
+++ movie-list/src/components/common/like.jsx
@@ -1,11 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Like extends React.Component {
-  render() { 
-    let classes="fa fa-heart";
+  render() {
+    let classes = "fa fa-heart";
     if (!this.props.liked) classes += "-o";
-    return (<i onClick={this.props.onLikeToggle} className={classes} aria-hidden="true"></i>);
+    return (
+      <i
+        onClick={this.props.onLikeToggle}
+        style={{ cursor: "pointer" }}
+        className={classes}
+        aria-hidden="true"
+      ></i>
+    );
   }
 }
- 
+
 export default Like;
