47fe0cb436b71ee1928c8924196455455e032b2c
Lomanfan <lfan.canada@gmail.com>
2021-12-09 03:42:31 -0700

    add pagination nav>ul.pagination>ls.page-item>a.page-link

--- movie-list/src/components/common/pagination.jsx
+++ movie-list/src/components/common/pagination.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Pagination = (props) => {
-  return ( null );
-}
- 
+  return (
+    <nav>
+      <ul className="pagination">
+        <li className="page-item">
+          <a className="page-link">1</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
 export default Pagination;
