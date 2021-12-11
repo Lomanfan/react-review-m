63d92e3941eb0f03951696f254851ce5824639f8
Lomanfan <lfan.canada@gmail.com>
2021-12-10 23:59:42 -0700

    add paginate function

---
+++ movie-list/src/utilities/paginate.js
@@ -0,0 +1,10 @@
+import _ from "lodash";
+
+export function paginate(items, pageNumber, pageSize) {
+  const startIndex = (pageNumber - 1) * pageSize;
+  return _(items).slice(startIndex).take(pageSize).value();
+  // _(items)  //convert items to a lodash obj
+  // _.slice(item, startIndex) //create an array
+  // _.take()  //to take items for the array
+  // .value()  //convert to lodash obj to a regular array
+}
