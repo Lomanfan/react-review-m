4022e0e1c4edc38d5664bc29ba03bed714bf3a37
Lomanfan <lfan.canada@gmail.com>
2021-12-12 22:47:05 -0700

    set ListGroup.defaultProps: textProperty and valueProperty for ListGroup reusability

--- movie-list/src/components/common/listGroup.jsx
+++ movie-list/src/components/common/listGroup.jsx
@@ -6,10 +6,17 @@ const ListGroup = (props) => {
   return (
     <ul className="list-group">
       {items.map((item) => (
-        <li key={item[valueProperty]} className="list-group-item">{item[textProperty]}</li>
+        <li key={item[valueProperty]} className="list-group-item">
+          {item[textProperty]}
+        </li>
       ))}
     </ul>
   );
+};
+
+ListGroup.defaultProps = {
+  textProperty: "name",
+  valueProperty: "_id",
 };
 
 export default ListGroup;
