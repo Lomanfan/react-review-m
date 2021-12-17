df4ee00fa8c262077ba3ca7d6645b28b485df586
Lomanfan <lfan.canada@gmail.com>
2021-12-17 12:15:43 -0700

    add sort icon

--- movie-list/src/components/common/tableHeader.jsx
+++ movie-list/src/components/common/tableHeader.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class TableHeader extends Component {
-    raiseSort = (path) => {
+  raiseSort = (path) => {
     const sortColumn = { ...this.props.sortColumn };
     if (sortColumn.path === path)
       sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
@@ -12,13 +12,29 @@ class TableHeader extends Component {
     this.props.onSort(sortColumn);
   };
 
-  render() { 
-    return <thead>
-      <tr>
-        {this.props.columns.map(column => <th key={column.path || column.key} onClick={() => this.raiseSort(column.path)}>{column.label}</th>)}
-      </tr>
-    </thead>;
+  renderSortIcon = column => {
+    const { sortColumn } = this.props;
+    if (column.path !== sortColumn.path) return null;
+    if (sortColumn.order === 'asc') return <i className="fa fa-sort-asc" />;
+    return <i className="fa fa-sort-desc"/>;
+  };
+
+  render() {
+    return (
+      <thead>
+        <tr>
+          {this.props.columns.map((column) => (
+            <th
+              key={column.path || column.key}
+              onClick={() => this.raiseSort(column.path)}
+            >
+              {column.label} {this.renderSortIcon(column)}
+            </th>
+          ))}
+        </tr>
+      </thead>
+    );
   }
 }
- 
+
 export default TableHeader;
