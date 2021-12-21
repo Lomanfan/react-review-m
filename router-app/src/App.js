79e685b11998a67ff97716d7eba339f1dc76fd31
Lomanfan <lfan.canada@gmail.com>
2021-12-20 20:25:45 -0700

    change year and month to optional params for the posting path; render year and month info

--- router-app/src/App.js
+++ router-app/src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
               path="/products"
               render={(props) => <Products {...props} sortBy="newest" />}
             />
-            <Route path="/posts/:year/:month" component={Posts} />
+            <Route path="/posts/:year?/:month?" component={Posts} />
             <Route path="/admin" component={Dashboard} />
             <Route path="/" component={Home} />
           </Switch>
