a6c219f473a5ec9f209381bc479719ea4cd01cdc
Lomanfan <lfan.canada@gmail.com>
2022-01-10 20:52:25 -0700

    add notes

--- router-app/src/components/posts.jsx
+++ router-app/src/components/posts.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import queryString from 'query-string';
 
-const Posts = ({match}) => {
+const Posts = ({match, location}) => {
+  //http://localhost:3000/posts/2018/06?sortBy=newest&approved=true
+  const {sortBy, approved} = queryString.parse(location.search);
+  console.log("sortBy",sortBy, "approved", approved);
+
   return (
     <div>
       <h1>Posts</h1>
