0c827b0e1488a3c69b1afb271daa6646faae12c4
Lomanfan <lfan.canada@gmail.com>
2022-02-05 14:55:00 -0700

    nested routing

--- router-app/src/components/admin/dashboard.jsx
+++ router-app/src/components/admin/dashboard.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom"
 
 const Dashboard = ({ match }) => {
   return (
     <div>
       <h1>Admin Dashboard</h1>
+      <ul>
+        <li>
+          <Link to="/admin/posts">Posts</Link>
+        </li>
+        <li>
+          <Link to="/admin/users">Users</Link>
+        </li>
+      </ul>
     </div>
   );
 };
