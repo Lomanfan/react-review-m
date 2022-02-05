7eca4205f5fc52fc0cd739b144427ec9257bdaf7
Lomanfan <lfan.canada@gmail.com>
2022-02-05 16:07:05 -0700

    add Redirect from home page ./ to ./movies

--- movie-list/src/App.js
+++ movie-list/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import React, { Component } from "react";
 import Movies from "./components/movies";
-import { Route } from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
@@ -10,10 +10,13 @@ class App extends React.Component {
   render() {
     return (
       <main className="container">
-        <Route path="/movies" component={Movies}></Route>
-        <Route path="/customers" component={Customers}></Route>
-        <Route path="/rentals" component={Rentals}></Route>
-        <Route path="/not-found" component={NotFound}></Route>
+        <Switch>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" to="/movies" />
+        </Switch>
       </main>
     );
   }
