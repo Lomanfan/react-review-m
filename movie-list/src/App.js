1969029c97eaf0f7e273b46e17a42aed58e46e17
Lomanfan <lfan.canada@gmail.com>
2022-02-05 16:02:19 -0700

    add routes to App.js

--- movie-list/src/App.js
+++ movie-list/src/App.js
@@ -1,12 +1,22 @@
 import "./App.css";
+import React, { Component } from "react";
 import Movies from "./components/movies";
+import { Route } from "react-router-dom";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
 
-function App() {
-  return (
-    <main className="container">
-      <Movies/>
-    </main>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <main className="container">
+        <Route path="/movies" component={Movies}></Route>
+        <Route path="/customers" component={Customers}></Route>
+        <Route path="/rentals" component={Rentals}></Route>
+        <Route path="/not-found" component={NotFound}></Route>
+      </main>
+    );
+  }
 }
 
 export default App;
