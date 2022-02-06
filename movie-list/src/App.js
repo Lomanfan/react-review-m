c7bcf1a6f21495acb62b430f4f042437d6776191
Lomanfan <lfan.canada@gmail.com>
2022-02-05 18:44:56 -0700

    add NavLink to navBar component, render NavBar on App.js

--- movie-list/src/App.js
+++ movie-list/src/App.js
@@ -1,24 +1,28 @@
-import "./App.css";
 import React, { Component } from "react";
-import Movies from "./components/movies";
 import { Route, Redirect, Switch } from "react-router-dom";
+import Movies from "./components/movies";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
+import "./App.css";
 
 class App extends React.Component {
   render() {
     return (
-      <main className="container">
-        <Switch>
-          <Route path="/movies" component={Movies} />
-          <Route path="/customers" component={Customers} />
-          <Route path="/rentals" component={Rentals} />
-          <Route path="/not-found" component={NotFound} />
-          <Redirect from="/" exact to="/movies" />
-          <Redirect to="/not-found" />
-        </Switch>
-      </main>
+      <>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </>
     );
   }
 }
--- movie-list/src/components/navBar.jsx
+++ movie-list/src/components/navBar.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import NavLink from 'react-router-dom/NavLink';
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="#">
+        <Link className="navbar-brand" to="/">
           MovieApp
         </Link>
         <button
@@ -22,28 +22,25 @@ const NavBar = () => {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <NavLink className="nav-link active" aria-current="page" to="#">
-                Home
+              <NavLink className="nav-link" aria-current="page" to="/movies">
+                Movies
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="#">
-                Features
+              <NavLink className="nav-link" to="/customers">
+                Customers
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link" to="#">
-                Pricing
+              <NavLink className="nav-link" to="/rentals">
+                Rentals
               </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link disabled">Disabled</NavLink>
             </li>
           </ul>
         </div>
       </div>
     </nav>
   );
-}
- 
+};
+
 export default NavBar;
