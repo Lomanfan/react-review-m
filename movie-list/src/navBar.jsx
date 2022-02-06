9adaf5e2281f91557f785e90e6498a7141e16120
Lomanfan <lfan.canada@gmail.com>
2022-02-05 18:33:30 -0700

    change a tag to NavLink, replace ref with to

--- movie-list/src/navBar.jsx
+++ movie-list/src/navBar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import NavLink from 'react-router-dom/NavLink';
 
 const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          Navbar
-        </a>
+        <Link className="navbar-brand" to="#">
+          MovieApp
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -21,22 +22,22 @@ const NavBar = () => {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
+              <NavLink className="nav-link active" aria-current="page" to="#">
                 Home
-              </a>
+              </NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <NavLink className="nav-link" to="#">
                 Features
-              </a>
+              </NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <NavLink className="nav-link" to="#">
                 Pricing
-              </a>
+              </NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link disabled">Disabled</a>
+              <NavLink className="nav-link disabled">Disabled</NavLink>
             </li>
           </ul>
         </div>
