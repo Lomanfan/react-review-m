1bbaa3f30ec28c993788d2e9be57e6dd21e10582
Lomanfan <lfan.canada@gmail.com>
2022-02-05 18:25:45 -0700

    add Switch and Redirect for /not-found

--- movie-list/src/App.js
+++ movie-list/src/App.js
@@ -11,11 +11,12 @@ class App extends React.Component {
     return (
       <main className="container">
         <Switch>
-          <Route path="/movies" component={Movies}></Route>
-          <Route path="/customers" component={Customers}></Route>
-          <Route path="/rentals" component={Rentals}></Route>
-          <Route path="/not-found" component={NotFound}></Route>
-          <Redirect from="/" to="/movies" />
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
         </Switch>
       </main>
     );
