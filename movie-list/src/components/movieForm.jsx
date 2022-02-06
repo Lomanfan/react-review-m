275bc5927ed482612d5c7596e739f74a45b398d4
Lomanfan <lfan.canada@gmail.com>
2022-02-05 19:20:13 -0700

    render movie title dynamically, add movieForm component, update App.js

--- movie-list/src/components/movieForm.jsx
+++ movie-list/src/components/movieForm.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const MovieForm = () => {
-  return <h1>Movie Form</h1>;
+const MovieForm = ({ match }) => {
+  return <h1>Movie Form {match.params.id} </h1>;
 };
 
 export default MovieForm;
