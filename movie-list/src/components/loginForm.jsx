7b42d0d0c9f5b0a6acffab9c2a5b63cd0155cd34
Lomanfan <lfan.canada@gmail.com>
2022-02-07 22:26:29 -0700

    distructure error out of Joi.validate()

--- movie-list/src/components/loginForm.jsx
+++ movie-list/src/components/loginForm.jsx
@@ -19,15 +19,15 @@ class LoginForm extends Component {
   };
 
   validate = () => {
-    const result = Joi.validate(this.state.account, this.schema, {
+    const {error} = Joi.validate(this.state.account, this.schema, {
       abortEarly: false,
     });
     // console.log(result);
 
-    if (!result.error) return null;
+    if (!error) return null;
 
     const errors = {};
-    for (let item of result.error.details) errors[item.path[0]] = item.message;
+    for (let item of error.details) errors[item.path[0]] = item.message;
     return errors;
   };
 
