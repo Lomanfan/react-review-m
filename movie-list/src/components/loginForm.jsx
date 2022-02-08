63a9f57749368c3d5cf120024c555b74fefc7000
Lomanfan <lfan.canada@gmail.com>
2022-02-07 21:03:32 -0700

    display errors using input.jsx, setState of errors to errors or {}

--- movie-list/src/components/loginForm.jsx
+++ movie-list/src/components/loginForm.jsx
@@ -29,9 +29,9 @@ class LoginForm extends Component {
     e.preventDefault();
 
     const errors = this.validate();
-    console.log(errors);
+    // console.log(errors);
 
-    this.setState({ errors });
+    this.setState({ errors: errors || {}});
     if (errors) return;
 
     //Call the server
@@ -45,7 +45,7 @@ class LoginForm extends Component {
   };
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
 
     return (
       <div>
@@ -56,12 +56,14 @@ class LoginForm extends Component {
             label="Username"
             value={account.username}
             onChange={this.handleChange}
+            error={errors.username}
           />
           <Input
             name="password"
             label="Password"
             value={account.password}
             onChange={this.handleChange}
+            error={errors.password}
           />
           <button className="btn btn-primary">Login</button>
         </form>
