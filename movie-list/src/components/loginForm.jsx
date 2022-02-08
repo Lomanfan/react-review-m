f691bd46d2aa053f1e7feea49eca179863a900f6
Lomanfan <lfan.canada@gmail.com>
2022-02-07 22:22:42 -0700

    validate loginForm using Joi

--- movie-list/src/components/loginForm.jsx
+++ movie-list/src/components/loginForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Joi from "joi-browser";
 import Input from "./common/input";
 
 class LoginForm extends Component {
@@ -12,17 +13,22 @@ class LoginForm extends Component {
   //   this.username.current.foc
   // }
 
+  schema = {
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
+  };
+
   validate = () => {
+    const result = Joi.validate(this.state.account, this.schema, {
+      abortEarly: false,
+    });
+    // console.log(result);
+
+    if (!result.error) return null;
+
     const errors = {};
-
-    const { account } = this.state;
-    if (account.username.trim() === "")
-      errors.username = "Username is required";
-
-    if (account.password.trim() === "")
-      errors.password = "Password is required";
-
-    return Object.keys(errors).length === 0 ? null : errors;
+    for (let item of result.error.details) errors[item.path[0]] = item.message;
+    return errors;
   };
 
   handleSubmit = (e) => {
